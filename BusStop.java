aecf5fb43686a396692a218eaa9df0c20612d80d
Adam Drummond <github@adamd.im>
2025-02-09 11:13:03 +0000

    added framework for xls data

---
+++ BusStop.java
@@ -0,0 +1,13 @@
+class BusStop {
+    public String StopNo;
+    public String Naptan;
+    public String Latitude;
+    public String Longitude;
+
+    public BusStop(String no, String nap, String lat, String lon) {
+        StopNo = no;
+        Naptan = nap;
+        Latitude = lat;
+        Longitude = lon;
+    }
+}
